1bd12b64793a63f38fbf07b6ced02c14038d4972
Marco Salinas <marco.salinas@digitalmarketsquare.com>
2016-06-06 20:49:43 +0930

    #comment papa parser in place and reading lines: Okay #time 25m

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,40 @@
-Template.home.rendered = function() {
 
+Template.home.created = function() {
+	Template.instance().uploading = new ReactiveVar( false );
 };
+
+
+Template.home.helpers({ 
+    uploading :  function () {
+                    return Template.instance().uploading.get();
+                }
+});
+
+
+Template.home.events({
+  'change [name="uploadCSV"]': function( event, template ) {
+    event.preventDefault();
+    console.log('Start payslip');
+    template.uploading.set( true );
+
+    Papa.parse( event.target.files[0], {
+      header: false,
+      complete: function( results, file ) {
+  		Meteor.call('parseUpload', results.data, function (error, response) {
+               if ( error ) {
+                 console.log( error.reason );
+               } else {
+                 template.uploading.set( false );
+                 Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
+               }
+  		    });
+      }
+    });
+	
+	
+  },
+  
+  'click .login': function() {
+    Meteor.loginWithTwitter();
+  }
+});
--- server/methods/items.js
+++ server/methods/items.js
@@ -1,5 +1,12 @@
 Meteor.methods({
   'Items.insert': function (params) {
     Items.insert(params);
+  },
+  'parseUpload': function (data) {
+	    check( data, Array );
+    for ( i = 0; i < data.length; i++ ) {
+		 item   = data[ i ]; 
+		console.log(item);
+	}
   }
 });
